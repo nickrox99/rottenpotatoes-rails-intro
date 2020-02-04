a39ce9132f1fd3ef93e9e3fb03f63f881224714c
nickrox99 <nick.cashiola@gmail.com>
2020-02-04 15:55:06 +0000

    added unique value selection for ratings [Nick 2/4/2020]

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,7 +8,9 @@ class Movie < ActiveRecord::Base
             # code adapted from: 
                 # 1. https://stackoverflow.com/questions/21186067/ruby-extracting-the-unique-values-per-key-from-an-array-of-hashes
                 # 2. https://guides.rubyonrails.org/active_record_basics.html
-            self.where(:all).pluck(all :ratings)
+                # 3. https://stackoverflow.com/questions/9658881/rails-select-unique-values-from-a-column
+            #return self.distinct.pluck(:rating)
+            return self.uniq.pluck(:rating)
             
         end
     end
